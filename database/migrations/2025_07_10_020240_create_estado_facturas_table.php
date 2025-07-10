bfd6b3440407b7a07ab4837c85e18a2b200f7f4e
Sebastian Cortez Silva <seba.crz.sv@gmail.com>
2025-07-09 22:06:27 -0400

    feat:Migraciones de estado factura realizadas, también incluye un seeder con los 8 posibles estados de factura

--- database/migrations/2025_07_10_020240_create_estado_facturas_table.php
+++ database/migrations/2025_07_10_020240_create_estado_facturas_table.php
@@ -9,22 +9,19 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('estado_facturas', function (Blueprint $table) {
-            $table->unsignedTinyInteger('id')->primary();   // 0,1,2,...
-            $table->string('nombre');                       // ‘Pendiente’, ‘Pagada’, …
-            $table->text('descripcion')->nullable();        // detalle opcional
+            $table->id();
             $table->timestamps();
         });
     }
-
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('estado_facturas');
     }
 };
